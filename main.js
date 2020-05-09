2570cf3239f598920a997247c32d7d161b66f782
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2020-05-09 18:18:29 -0500

    Missing Syntax
    I was receiving a syntax error within the JS file due to missing a comma on line two after the object. Adding the comma corrected the error message allowing the application to run as it should.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const api = {
-    key: "54e6965a6aef2f55b313d4ee2c389024"
-    baseurl: "https://api.openweathermap.org/data/2.5"
+    key: "54e6965a6aef2f55b313d4ee2c389024",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -28,13 +28,13 @@ function displayResults (weather) {
   date.innerText = dateBuilder(now);
 
   let temp = document.querySelector('.current .temp');
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
 
   let weather_el = document.querySelector('.current .weather');
   weather_el.innerText = weather.weather[0].main;
 
   let hilow = document.querySelector('.hi-low');
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
 }
 
 function dateBuilder (d) {
